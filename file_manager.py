7b93496d9a3840ab82c962791eb7bd80a6a9dda6
KCprsnlcc <kcpersonalacc@gmail.com>
2025-05-08 15:48:45 +0800

    Importing Necessary Modules

---
+++ file_manager.py
@@ -0,0 +1,5 @@
+import sys
+import shutil
+from PySide6.QtCore import Qt, QDir, QFileInfo, QUrl
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileSystemModel, QTreeView, QVBoxLayout, QWidget, QPushButton, QLineEdit, QMenu, QAction, QFileDialog, QLabel, QHBoxLayout
+from PySide6.QtGui import QIcon, QPixmap
